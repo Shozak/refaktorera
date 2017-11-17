34df34257c9a1e5d3e6d7e672d95d0551e50efcf
Unknown <mazall593@hotmail.com>
2017-11-17 11:30:35 +0100

    first fix

--- main2.c
+++ main2.c
@@ -24,61 +24,44 @@ AKTIV EFFEKT 3-FAS ~: P=U*I*sqrt(3)*cos()
 #include <math.h>
 #include <limits.h>
 #include <stdbool.h>
-#include "funktioner.h"
+//#include "funktioner.h"
+bool continue_global;
 
 double ohms_lag(double r, double i){
-
     double u = i * r;
-
     return u;
 }
 
 double res_tot(double r1, double r2, double r3){
-
-
     double rtot= (1/r1) + (1/r2) + (1/r3);
     rtot= 1/rtot;
-
     return rtot;
 }
 
 double eff_enk(double u, double i){
-
     double p = u * i;
-
     return p;
 }
 
 double sken_eff(double u, double i){
-
     double s = u * i;
-
     return s;
-
 }
 
 double aktiv_eff(double u, double i, double cos){
-
     double p = u * i * cos;
-
     return p;
-
 }
 
 double sken_3fas(double u, double i){
-
     double s = u * i * sqrt(3);
-
     return s;
 }
 
 double aktiv_3fas(double u, double i, double cos){
-
     double p = u * i * sqrt(3) * cos;
-
     return p;
 }
-
 
 void print_menu(){
         printf("\n");
@@ -92,142 +75,155 @@ void print_menu(){
         printf("V�lj 7 f�r: AKTIV EFFEKT 3-FAS\n");
         printf("V�lj 0 f�r: F�R ATT AVSLUTA\n");
 }
-val_menu_1(){
+double print_resistans(){
+    double r;
+    printf("Skriv resistans R < 20 000ohm: \n ");
+    scanf("%lf", &r);
+    return r;
+}
+double print_ampere(){
+    double i;
+    printf("Skriv str�m I < 440 Ampere: \n");
+    scanf("%lf", &i);
+    return i;
+}
+double print_volts(){
+    double u;
+    printf("Skriv sp�nnngen U i volt(V): \n ");
+    scanf("%lf", &u);
+    return u;
+}
+double print_cos(){
+    double cos;
+    printf("Skriv in effektfaktorn cos > 0 && cos < 1:\n");
+    scanf("%lf", &cos);
+    return cos;
+}
+void print_fel(){
+        printf("F�r h�gt v�rde, f�rs�k igen: \n");
+}
+
+int val_menu_1(){
     printf("Ohms lag sp�nningen(volt/V) bet�ckning U lika med Resistansen(Ohm) bet�ckning R \n");
     printf("g�nger Str�mmen(Ampere) med bet�ckningen I. Kort U=R*I. \n\n");
     double r, i;
-    printf("Skriv resistans R < 20 000ohm: \n ");
-    scanf("%lf", &r);
+    r=print_resistans();
     if(r > 20000){
-        printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+        print_fel();
+        return 1;
     }
-    printf("Skriv str�m I < 440 Ampere: \n");
-    scanf("%lf", &i);
+    i=print_ampere();
     if(i > 440){
-        printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+        print_fel();
+        return 1;
     }
     printf("%f V\n", ohms_lag(r, i));
+    return 0;
 }
-val_menu_2(){
+int val_menu_2(){
     printf("Resistans sammankopplade i parallella kretsar �r lika med 1 delat Resistans R total �r lika med\n");
     printf("Resistans 1/R1 + 1/R2 + 1/R3 d� vi h�gst anv�nder tre resistanser.\n\n");
     double r1,r2,r3;
-    printf("Skriv resistans R1 < 20 000ohm: \n ");
-    scanf("%lf", &r1);
+    r1=print_resistans();
     if(r1 > 20000){
-        printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+        print_fel();
+        return 1;
     }
-    printf("Skriv resistans R2 < 20 000ohm: \n ");
-    scanf("%lf", &r2);
+    r2=print_resistans();
     if(r2 > 20000){
-        printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+        print_fel();
+        return 1;
     }
-    printf("Skriv resistans R3 < 20 000ohm: \n ");
-    scanf("%lf", &r3);
+    r3=print_resistans();
     if(r3 > 20000){
-        printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+        print_fel();
+        return 1;
     }
     printf("%f Ohm\n", res_tot(r1, r2, r3));
-
+    return 0;
 }
-val_menu_3(){
+int val_menu_3(){
     printf("Effektlagen enkel f�r likstr�m �r effekten P i Watt (W) lika med sp�nningen U i volt(V)\n");
     printf("g�nger str�mmen I i Ampere(A): \n\n");
     double u, i;
-    printf("Skriv sp�nnngen U i volt(V): \n ");
-    scanf("%lf", &u);
-    printf("Skriv str�m Ampere I < 440A: \n");
-    scanf("%lf", &i);
-    if(i > 440)
-    {
-        printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+    u=print_volts();
+    i=print_ampere();
+    if(i > 440){
+        print_fel();
+        return 1;
     }
     printf("%f W\n", eff_enk(u, i));
+    return 0;
 }
-val_menu_4(){
+int val_menu_4(){
     printf("Skenbar effekt enfas r�knas med storheten VA(VoltAmpere) som �r lika med sp�nningen U i volt(V)\n");
     printf("g�nger str�mmen I i ampere(A)\n\n");
     double u, i;
-    printf("Skriv Sp�nningen U i volt: \n ");
-    scanf("%lf", &u);
-    printf("Skriv str�m I < 440A: \n");
-    scanf("%lf", &i);
-    if(i > 440)
-    {
-        printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+    u=print_volts();
+    i=print_ampere();
+    if(i > 440){
+        print_fel();
+        return 1;
     }
     printf("%f VA\n", sken_eff(u, i));
-
+    return 0;
 }
-val_menu_5(){
+int val_menu_5(){
     printf("Aktiv medelefdekt enfas �r lika med effekt P i watt(W) lika med sp�nningen U i volt(V) g�nger str�mmen I \n");
     printf("i Ampere g�nger cosinus fi/efkektfaktor < 1:\n\n");
     double u, i, cos;
-    printf("Skriv sp�nning U i volt: \n ");
-    scanf("%lf", &u);
-    printf("Skriv str�m I: \n");
-    scanf("%lf", &i);
+    u=print_volts();
+    i=print_ampere();
     if(i > 440){
-        printf("F�r h�gt v�rde, f�rs�k igen:\n");
-        continue;
+        print_fel();
+        return 1;
     }
-    printf("Skriv in effektfaktorn cos > 0 && cos < 1:\n");
-    scanf("%lf", &cos);
+    cos=print_cos();
     if (cos < 0 && cos > 1){
         printf("Fel v�rde, f�rs�k igen\n");
-        continue;
+        return 1;
     }
     printf("%f W\n", aktiv_eff(u, i, cos));
+    return 0;
 }
-val_menu_6(){
+int val_menu_6(){
     printf("3-fas skenbar effekt �r v�xelsp�nning �r skenbar effekt S i voltampere(VA) lika med sp�nningen U i volt(V) \n");
     printf("g�nger str�mmen I i ampere(A) g�nger roten ur 3 SQRT(3).\n\n");
     double u, i;
-    printf("Skriv sp�nning U i volt(V) < 400V: \n ");
-    scanf("%lf", &u);
+    u=print_volts();
     if(u > 400){
         printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+        return 1;
     }
-    printf("Skriv str�m I i ampere < 440: \n");
-    scanf("%lf", &i);
+    i=print_ampere();
     if(i > 440){
-        printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+        print_fel();
+        return 1;
     }
     printf("%f VA\n", sken_3fas(u, i));
+    return 0;
 }
-val_menu_7(){
+int val_menu_7(){
     printf("3-fas aktiv effekt �r effekten P i Watt(W) lika med sp�nningen U i volt(V) g�nger str�mmen I i ampere(A)\n");
     printf("g�nger cos < 1 && cos > 0 g�nger roten ur 3 SQRT(3).\n\n");
     double u, i, cos;
-    printf("Skriv Sp�nningen U i volt(V): \n ");
-    scanf("%lf", &u);
+    u=print_volts();
     if(u > 400){
-        printf("F�r h�gt v�rde, f�rs�k igen.\n");
-        continue;
+        print_fel();
+        return 1;
     }
-    printf("Skriv str�m I i ampere(A): \n");
-    scanf("%lf", &i);
+    i=print_ampere();
     if(i > 440){
-        printf("F�r h�gt v�rde, f�rs�k igen.\n");
-        continue;
+        print_fel();
+        return 1;
     }
-
-    printf("Skriv in effektfaktorn cos > 0 && cos < 1: \n");
-    scanf("%lf", &cos);
+    cos=print_cos();
     if (cos < 0 && cos > 1){
-        printf("F�r h�gt v�rde, f�rs�k igen: \n");
-        continue;
+        print_fel();
+        return 1;
     }
     printf("%f W\n", aktiv_3fas(u ,i, cos));
+    return 0;
 }
 
 int main()
@@ -242,25 +238,39 @@ int main()
         scanf("%d", &val);
 
         if(val == 1){
-            val_menu_1();
+            if(val_menu_1()){
+                continue;
+            }
         }
         else if(val == 2){
-            val_menu_2();
+            if(val_menu_2()){
+                continue;
+            }
         }
         else if(val == 3){
-            val_menu_3();
+            if(val_menu_3()){
+                continue;
+            }
         }
         else if(val == 4){
-            val_menu_4();
+            if(val_menu_4()){
+                continue;
+            }
         }
         else if(val == 5){
-            val_menu_5();
+            if(val_menu_5()){
+                continue;
+            }
         }
         else if(val == 6){
-            val_menu_6();
+            if(val_menu_6()){
+                continue;
+            }
         }
         else if(val == 7){
-            val_menu_7();
+            if(val_menu_7()){
+                continue;
+            }
         }
         else if (val == 0){
             exit = true;
