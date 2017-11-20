ded1aa824bf2c706797088c54350be92d15af937
Unknown <mazall593@hotmail.com>
2017-11-20 08:41:28 +0100

    filnal

---
+++ funktioner.h
@@ -0,0 +1,23 @@
+
+
+
+double ohms_lag(double r, double i);
+double res_tot(double r1, double r2, double r3);
+double eff_enk(double u, double i);
+double sken_eff(double u, double i);
+double aktiv_eff(double u, double i, double cos);
+double sken_3fas(double u, double i);
+double aktiv_3fas(double u, double i, double cos);
+void print_menu(void);
+double print_resistans(void);
+double print_ampere(void);
+double print_volts(void);
+double print_cos(void);
+void print_fel(void);
+int val_menu_1(void);
+int val_menu_2(void);
+int val_menu_3(void);
+int val_menu_4(void);
+int val_menu_5(void);
+int val_menu_6(void);
+int val_menu_7(void);
